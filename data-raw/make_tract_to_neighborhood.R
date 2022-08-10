eb3f4e351612f9ceac36b436d23741740b3efa62
Erika <erika.rasnick@gmail.com>
2022-08-10 10:55:41 -0400

    save tract to neighborhood as internal data
    Merge branch 'crosswalks' of https://github.com/geomarker-io/cincy into crosswalks
    
    # Conflicts:
    #	data-raw/make_tract_to_neighborhood.R

--- data-raw/make_tract_to_neighborhood.R
+++ data-raw/make_tract_to_neighborhood.R
@@ -4,4 +4,4 @@ hamilton_tract_to_cincy_neighborhood <-
   read_csv('data-raw/hamilton_tract_to_cincy_neighborhood.csv')
 
 usethis::use_data(hamilton_tract_to_cincy_neighborhood,
-                  overwrite = TRUE)
+                  overwrite = TRUE, internal = TRUE)
