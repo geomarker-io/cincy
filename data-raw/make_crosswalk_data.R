4167750ca17de77509c73a8eb899fd5f02cbc8c4
Erika <erika.rasnick@gmail.com>
2022-08-31 09:27:53 -0400

    actually save internal data

--- data-raw/make_crosswalk_data.R
+++ data-raw/make_crosswalk_data.R
@@ -25,11 +25,6 @@ tract_to_zcta_2010 <- make_weights(zcta_tigris_2010, tract_tigris_2010, key = zc
 zcta_to_tract_2020 <- make_weights(tract_tigris_2020, zcta_tigris_2020, key = tract_fips)
 tract_to_zcta_2020 <- make_weights(zcta_tigris_2020, tract_tigris_2020, key = zcta)
 
-usethis::use_data(zcta_to_tract_2000, tract_to_zcta_2000,
-                  zcta_to_tract_2010, tract_to_zcta_2010,
-                  zcta_to_tract_2020, tract_to_zcta_2020,
-                  overwrite = TRUE, internal = TRUE)
-
 # tract to tract
 tracts_2000_to_2010 <- make_weights(tract_tigris_2010 %>%
                                       rename(tract_fips_2010 = tract_fips),
@@ -66,11 +61,6 @@ tracts_2020_to_2000 <- make_weights(tract_tigris_2000 %>%
                                     tract_tigris_2020 %>%
                                       rename(tract_fips_2020 = tract_fips),
                                     key = tract_fips_2000)
-
-usethis::use_data(tracts_2000_to_2010, tracts_2010_to_2000,
-                  tracts_2010_to_2020, tracts_2020_to_2010,
-                  tracts_2000_to_2020, tracts_2020_to_2000,
-                  overwrite = TRUE, internal = TRUE)
 
 # zcta to zcta
 zctas_2000_to_2010 <- make_weights(zcta_tigris_2010 %>%
@@ -109,7 +99,13 @@ zctas_2020_to_2000 <- make_weights(zcta_tigris_2000 %>%
                                       rename(zcta_2020 = zcta),
                                     key = zcta_2000)
 
-usethis::use_data(zctas_2000_to_2010, zctas_2010_to_2000,
+usethis::use_data(zcta_to_tract_2000, tract_to_zcta_2000,
+                  zcta_to_tract_2010, tract_to_zcta_2010,
+                  zcta_to_tract_2020, tract_to_zcta_2020,
+                  tracts_2000_to_2010, tracts_2010_to_2000,
+                  tracts_2010_to_2020, tracts_2020_to_2010,
+                  tracts_2000_to_2020, tracts_2020_to_2000,
+                  zctas_2000_to_2010, zctas_2010_to_2000,
                   zctas_2010_to_2020, zctas_2020_to_2010,
                   zctas_2000_to_2020, zctas_2020_to_2000,
                   overwrite = TRUE, internal = TRUE)
