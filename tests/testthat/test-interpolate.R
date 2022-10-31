aa45cc24c1429664fc931ae15f758dd836fd248c
Cole Brokamp <cole-brokamp@users.noreply.github.com>
2022-10-31 14:09:38 -0400

    update county docs, fix county id typo; closes #16; add vintage year to neighborhood definitions (#25)
    * update county docs, fix county id typo; closes #16
    
    * use GHA to build pages automatically
    
    * v0.3.1 release
    
    * update gha R CMD check to v2
    
    * change how cchmc neighborhood data works (add vintage year)
    
    * fix broken documentation for county data

--- tests/testthat/test-interpolate.R
+++ tests/testthat/test-interpolate.R
@@ -2,6 +2,6 @@ test_that("interpolate", {
   dep_index_zcta <- interpolate(dep_index, cincy::zcta_tigris_2010, "pop")
   expect_snapshot(dep_index_zcta)
   dep_index_zcta$n_things <- 1:nrow(dep_index_zcta)
-  dep_index_neigh <- interpolate(dep_index_zcta, cincy::neigh_cchmc, "homes")
+  dep_index_neigh <- interpolate(dep_index_zcta, cincy::neigh_cchmc_2010, "homes")
   expect_snapshot(dep_index_neigh)
 })
